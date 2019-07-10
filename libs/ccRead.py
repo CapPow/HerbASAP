d19b7786b42ff7515f069e5637f9189612489c18
Dakila <bgq527@mocs.utc.edu>
2019-07-10 16:30:52 -0400

    Removal of extract patches

--- libs/ccRead.py
+++ libs/ccRead.py
@@ -230,60 +230,6 @@ class ColorchipRead:
         except SystemError as e:
             print(f"System error: {e}")
 
-    def extract_patches(self, arr, patch_shape=125, extraction_step=25):
-        """Extracts patches of any n-dimensional array in place using strides.
-        Given an n-dimensional array it will return a 2n-dimensional array with
-        the first n dimensions indexing patch position and the last n indexing
-        the patch content. This operation is immediate (O(1)). A reshape
-        performed on the first n dimensions will cause numpy to copy data, leading
-        to a list of extracted patches.
-        Read more in the :ref:`User Guide <image_feature_extraction>`.
-        Parameters
-        ----------
-        arr : ndarray
-            n-dimensional array of which patches are to be extracted
-        patch_shape : integer or tuple of length arr.ndim
-            Indicates the shape of the patches to be extracted. If an
-            integer is given, the shape will be a hypercube of
-            sidelength given by its value.
-        extraction_step : integer or tuple of length arr.ndim
-            Indicates step size at which extraction shall be performed.
-            If integer is given, then the step is uniform in all dimensions.
-        Returns
-        -------
-        patches : strided ndarray
-            2n-dimensional array indexing patches on first n dimensions and
-            containing patches on the last n dimensions. These dimensions
-            are fake, but this way no data is copied. A simple reshape invokes
-            a copying operation to obtain a list of patches:
-            result.reshape([-1] + list(patch_shape))
-        
-        Function adapted from: https://github.com/diacaf/image-enhance-keras/blob/master/imgpatch.py
-        """
-
-        arr_ndim = arr.ndim
-
-        if isinstance(patch_shape, numbers.Number):
-            patch_shape = tuple([patch_shape] * arr_ndim)
-        if isinstance(extraction_step, numbers.Number):
-            extraction_step = tuple([extraction_step] * arr_ndim)
-
-        patch_strides = arr.strides
-
-        slices = [slice(None, None, st) for st in extraction_step]
-        indexing_strides = arr[slices].strides
-
-        patch_indices_shape = ((np.array(arr.shape) - np.array(patch_shape)) //
-                               np.array(extraction_step)) + 1
-
-        shape = tuple(list(patch_indices_shape) + list(patch_shape))
-        strides = tuple(list(indexing_strides) + list(patch_strides))
-
-        patches = as_strided(arr, shape=shape, strides=strides,
-                             writeable=False)
-
-        return patches
-
     def process_colorchip_small(self, im, original_size, stride_style='quick',
                                 stride=25, partition_size=125, buffer_size=10,
                                 over_crop=0, high_precision=False):
