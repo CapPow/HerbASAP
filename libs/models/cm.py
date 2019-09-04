611b536ea3b9e1f2511eb7f0938bcef5bb2421ff
Dakila L <bgq527@mocs.utc.edu>
2019-09-04 11:09:38 -0400

    Commit new discriminator network (more accurate on representative dataset)

--- libs/models/cm.py
+++ libs/models/cm.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 
-converter = tf.lite.TFLiteConverter.from_keras_model_file("mlp_proposal_s2.hdf5")
+converter = tf.lite.TFLiteConverter.from_keras_model_file("discriminator.hdf5")
 tflite_model = converter.convert()
-open("mlp_proposal_s2.tflite", "wb").write(tflite_model)
+open("discriminator.tflite", "wb").write(tflite_model)
