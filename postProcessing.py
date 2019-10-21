68b6d219bc0648673b29439b47a9b5da06b6e5f1
bgq527 <bgq527@mocs.utc.edu>
2019-10-21 08:51:04 -0400

    Update icon to alternate icon to match Jekyll site

--- postProcessing.py
+++ postProcessing.py
@@ -182,7 +182,7 @@ class appWindow(QMainWindow):
         # initiate the persistant settings
         # todo update this when name is decided on
         self.settings = QSettings('HerbASAP', 'HerbASAP')
-        self.setWindowIcon(QtGui.QIcon('docs/icon.png'))
+        self.setWindowIcon(QtGui.QIcon('docs/icon_a.png'))
         self.settings.setFallbacksEnabled(False)  # File only, no fallback to registry.
         # populate the settings based on the previous preferences
         self.populateSettings() # this also loads in the settings profile
